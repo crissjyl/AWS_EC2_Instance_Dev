8be36a35a19ba71aa738f665d5599969cde168b6
crissjyl <cliu713@gmail.com>
2020-02-16 14:25:11 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,18 +4,8 @@ provider "aws" {
   region = "us-east-2b"
  }
 
-module "vpc" {
-  source = "../../"
 
-  create_vpc = false
-
-  manage_default_vpc               = true
-  default_vpc_name                 = "default"
-  default_vpc_enable_dns_hostnames = true
-}
  
-
-
 resource "aws_instance" "demo" {
   ami = "ami-0e70c2dc2489eb7ff"
   instance_type = "t2.micro"
