929dba7fd506d812a81ddb3fd74aa0b350d697cc
crissjyl <cliu713@gmail.com>
2020-02-16 13:46:21 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,15 @@
 provider "aws" {
-  profile = "launch-wizard-1"
+  profile = "default"
   region = "us-east-2b"
  }
  
+resource "aws_vpc" "demo" {
+  cidr_block = "3.12.49.157"
+
 resource "aws_instance" "demo" {
   ami = "ami-0e70c2dc2489eb7ff"
   instance_type = "t2.micro"
 }
+
+
   
